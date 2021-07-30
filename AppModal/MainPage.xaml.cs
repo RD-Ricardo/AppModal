31b7bd968a94555070de919e95b539063fd0bd99
RD-Ricardo <ricardodias169@gmail.com>
2021-07-30 17:54:49 -0300

    Adicionar arquivos de projeto.

---
+++ AppModal/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppModal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AbrirModal(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(
+                new Modal()
+            );
+        }
+    }
+}
